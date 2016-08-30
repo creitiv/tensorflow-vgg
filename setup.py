432bea8b450f09379ab8bd28b8e52728adc576c5
Kristian Klemon <kristian@outbank.io>
2016-08-30 14:11:32 +0200

    Reorganize project as package
    Reorganizes the projects file structure as Python package and adds a setup.py and MANIFEST.in.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name = "tfvgg",
+    version = "1.0",
+    description = "Tensorflow implementation of VGG 16 and VGG 19",
+    author = "machrisaa",
+    url = "https://github.com/machrisaa/tensorflow-vgg",
+    packages = ["tfvgg"]
+)
---
+++ tfvgg/__init__.py
@@ -0,0 +1,2 @@
+from .vgg16 import Vgg16
+from .vgg19 import Vgg19
